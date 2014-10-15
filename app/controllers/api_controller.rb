d7d128dd61d8b19c3b78ed4271366d0af251b2ef
Fernando Varani <fervarani@gmail.com>
2014-10-15 14:45:20 -0300

    fix quotations

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,7 +11,8 @@ class ApiController < ActionController::Base
     end
 
     quotations = @shop.quote(request_params)
-    quotations << forward_quote
+    quotations += forward_quote
+
     render json: quotations || {}
   end
 
