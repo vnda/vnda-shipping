457f042d4ece397576f54630d5297deaff5323d2
Fernando Varani <fervarani@gmail.com>
2014-10-15 09:49:29 -0300

    add seed for delivery type

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+DeliveryType.find_or_create_by!(name: "Normal", enabled: true)
+DeliveryType.find_or_create_by!(name: "Expressa", enabled: true)
