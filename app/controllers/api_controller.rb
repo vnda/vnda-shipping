bc76da037cbded9b5c0020c9e9aef0b6cad55af8
Fernando Varani <fervarani@gmail.com>
2014-10-17 10:52:08 -0300

    Fix for nil foward_quote

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,7 +11,7 @@ class ApiController < ActionController::Base
     end
 
     quotations = @shop.quote(request_params)
-    quotations += forward_quote
+    quotations += forward_quote if forward_quote
 
     render json: quotations || {}
   end
