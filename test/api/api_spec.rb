cb3f7db506838e4c34f982ef2d9ed75c061135a6
Fernando Varani <fervarani@gmail.com>
2014-10-22 11:38:25 -0200

    Api quote test

--- test/api/api_spec.rb
+++ test/api/api_spec.rb
@@ -5,24 +5,21 @@ class ApiSpec < ActionDispatch::IntegrationTest
   setup do
    @shop = shops(:one)
    @shipping_method = shipping_methods(:one)
-   @shipping_method.min_weigth = 10
-   @shipping_method.max_weigth = 100
-   @shipping_method.save
+   @shipping_method.zip_rules.create!([
+         { range: 0..99999999, price: 15.0, deadline: 2 }
+       ])
   end
 
-  let(:zip_rule_params) { { shipping_method: @shipping_method, price: 10.00, deadline: 2, range: Range.new(10,100) } }
-  let(:zip_rule) { ZipRule.create zip_rule_params }
-
-  describe "items that are viewable by this user" do
-
+  describe "api quote" do
     it "returns available methods" do
 
-      params = JSON.parse('{"origin_zip":"90540140","shipping_zip":"92200290","order_total_price":10.0,"aditional_deadline":null,"aditional_price":null,"products":[{"sku":"CSMT-1","price":10.0,"height":2,"length":16,"width":11,"weight":0.001}]}')
+      params = JSON.parse('{"origin_zip":"12946636","shipping_zip":"92200290","order_total_price":10.0,"aditional_deadline":null,"aditional_price":null,"products":[{"sku":"CSMT-1","price":10.0,"height":2,"length":16,"width":11,"weight":10}]}')
       post "/quote?token=#{@shop.token}", params
 
+      response.status.must_equal 200
+      response.body.must_equal '[{"name":"Metodo 1","price":15.0,"deadline":2,"express":true,"slug":"metodo-1"}]'
 
     end
-
   end
 
 
