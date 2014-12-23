db28306d3496c5cf4a9e3059718ffa151b7eeef6
Francisco DC <fdc@razaoinfo.com.br>
2014-12-23 16:43:07 -0200

    validate presence of delivery type

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -19,7 +19,7 @@ class ShippingMethod < ActiveRecord::Base
   has_many :zip_rules, dependent: :destroy
   accepts_nested_attributes_for :zip_rules, allow_destroy: true, reject_if: :all_blank
 
-  validates :name, presence: true
+  validates :name, :delivery_type_id, presence: true
 
   before_save :generate_slug, if: :name_changed?
 
