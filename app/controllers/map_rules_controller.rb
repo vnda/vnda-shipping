da000ab89e57d5f10187c1d4b2da89a32ac3ade1
Otavio Medeiros <otaviorm@gmail.com>
2016-04-05 17:47:14 +0000

    form to download KML with error message when it is not found

--- app/controllers/map_rules_controller.rb
+++ app/controllers/map_rules_controller.rb
@@ -1,3 +1,5 @@
+require 'rest_client'
+
 class MapRulesController < ApplicationController
 
   def index
@@ -5,6 +7,22 @@ class MapRulesController < ApplicationController
     @method = ShippingMethod.find(params[:shipping_method_id])
 
     @map_rules = []  
+  end
+
+  def download_kml
+    begin
+      response = RestClient.get 'https://www.google.com/maps/d/kml', {params: {mid: params[:shipping_method][:mid], forcekml: '1'}}
+      xml_doc  = Nokogiri::XML(response)
+      
+      @polygons = xml_doc.css('Document Folder Placemark').collect do |placemark|
+        #name: placemark.css('name').text, coordinates: placemark.css('Polygon coordinates').text
+      end
+
+      @map_rules = []      
+    rescue RestClient::ResourceNotFound => e
+      flash.now[:error] = "Mapa não encontrado para MID: #{params[:shipping_method][:mid]}"
+    end
+
   end
 
 end
