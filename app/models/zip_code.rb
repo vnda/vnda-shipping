a935f2c75f8652a6e8c8e3962ba88837b89a0670
Otavio Medeiros <otaviorm@gmail.com>
2016-04-12 19:43:34 +0000

    using geocoding api key as environment variable

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -1,7 +1,9 @@
 class ZipCode
 
+  GMAPS_GEOCODING_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+
   def self.get_geolocation_for(zip_code)
-    response = RestClient.get 'http://maps.googleapis.com/maps/api/geocode/json', {params: {address: zip_code, region: 'br'}}
+    response = RestClient.get GMAPS_GEOCODING_API_URL, {params: {address: zip_code, region: 'br', key: ENV['GMAPS_GEOCODING_API_KEY']}}
     response = JSON.parse(response).with_indifferent_access
     response[:status].eql?('ZERO_RESULTS') ? {lng: 0, lat: 0} : response[:results].first[:geometry][:location]   
   end
