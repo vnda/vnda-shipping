2d56c851bdcebe068f3bffd9dc5e1fd3568a05cf
Otavio Medeiros <otaviorm@gmail.com>
2016-06-02 18:25:58 +0000

    feature to delete a place in shipping method crud

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,11 +7,17 @@ class PlacesController < ApplicationController
     @method = ShippingMethod.find(params[:shipping_method_id])
 
     begin
-      @method.check_and_update_places
+      @method.check_and_update_places if @method.places.empty?
       @places = @method.places.order('id asc')
     rescue RestClient::Unauthorized
       flash.now[:'vnda-places-error'] = 'Loja não possui cadastro no vnda-places'
     end
   end
 
+  def destroy
+    Place.find(params[:id]).destroy
+    @shipping_method = ShippingMethod.find(params[:shipping_method_id])
+    @places = @shipping_method.places.order('id asc')
+    render :index
+  end
 end
