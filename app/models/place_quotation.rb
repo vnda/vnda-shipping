c5fb7cf1de146972b7298eff58a81e679964ed7a
Otavio Medeiros <otaviorm@gmail.com>
2016-06-07 18:35:03 +0000

    shipping method id in places json

--- app/models/place_quotation.rb
+++ app/models/place_quotation.rb
@@ -1,8 +1,9 @@
-PlaceQuotation = Struct.new(:name, :delivery_type, :delivery_type_slug, :price) do
-  def initialize(name:, delivery_type:)
+PlaceQuotation = Struct.new(:name, :delivery_type, :delivery_type_slug, :price, :shipping_method_id) do
+  def initialize(name:, delivery_type:, shipping_method_id:)
     self.name = name
     self.delivery_type = delivery_type || ''
     self.delivery_type_slug = 'places'
     self.price = 0
+    self.shipping_method_id = shipping_method_id
   end
 end
