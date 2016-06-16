cda00ba839884c390bfc05b1c18226b78f2c3ab9
Francisco DC <fdc@razaoinfo.com.br>
2016-06-16 11:37:18 -0300

    show quotations details by cart id

--- app/controllers/quote_histories_controller.rb
+++ app/controllers/quote_histories_controller.rb
@@ -7,10 +7,19 @@ class QuoteHistoriesController < ApplicationController
   end
 
   def show
+    if @quote
+      render :layout => false if params[:cart_id].present?
+    else
+      render "not_found", :layout => false
+    end
   end
 
   private
     def set_period
-      @quote = @shop.quotes.find(params[:id])
+      if params[:cart_id].present?
+        @quote = @shop.quotes.where(cart_id: params[:cart_id].to_i).order("updated_at desc").first
+      else
+        @quote = @shop.quotes.find(params[:id])
+      end
     end
 end
