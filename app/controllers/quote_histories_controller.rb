b69378a0d6a2e738aae841ca0f8390bd5ba64d2a
Francisco DC <fdc@razaoinfo.com.br>
2016-06-16 12:40:32 -0300

    auth with tokena or password

--- app/controllers/quote_histories_controller.rb
+++ app/controllers/quote_histories_controller.rb
@@ -1,6 +1,8 @@
 class QuoteHistoriesController < ApplicationController
   before_filter { @shop = Shop.find(params[:shop_id]) }
   before_action :set_period, only: [:show]
+  before_action :require_authentication, except: :show
+  before_action :authenticate_with_token_or_password, only: :show
 
   def index
     @quotes = @shop.quotes.order("updated_at desc")
@@ -20,6 +22,20 @@ class QuoteHistoriesController < ApplicationController
         @quote = @shop.quotes.where(cart_id: params[:cart_id].to_i).order("updated_at desc").first
       else
         @quote = @shop.quotes.find(params[:id])
+      end
+    end
+
+    def authenticate_with_token_or_password
+      if params[:token].present?
+        begin
+          Shop.find_by!(token: params[:token])
+        rescue ActiveRecord::RecordNotFound
+          return head :unauthorized
+        end
+      else
+        authenticate_or_request_with_http_basic do |name, password|
+          name == ENV['HTTP_USER'] && password == ENV['HTTP_PASSWORD']
+        end
       end
     end
 end
