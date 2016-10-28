d09e9b7e956a2dafab37a28a4c759d5caf131389
Joel Souza <joel.souza@gmail.com>
2016-10-28 12:06:08 -0200

    Fix place quotation with notice attributte

--- app/models/place_quotation.rb
+++ app/models/place_quotation.rb
@@ -1,11 +1,12 @@
-PlaceQuotation = Struct.new(:name, :delivery_type, :deadline, :delivery_type_slug, :price, :shipping_method_id, :slug) do
-  def initialize(name:, delivery_type:, shipping_method_id:, slug:, deadline:)
+PlaceQuotation = Struct.new(:name, :delivery_type, :deadline, :delivery_type_slug, :price, :shipping_method_id, :slug, :notice) do
+  def initialize(name:, delivery_type:, shipping_method_id:, slug:, deadline:, notice:,)
     self.name = name
     self.delivery_type = delivery_type || ''
     self.delivery_type_slug = 'places'
     self.price = 0
     self.deadline = deadline
     self.shipping_method_id = shipping_method_id
-    self.slug = slug
+    self.slug = slug,
+    self.notice = notice || ''
   end
 end
