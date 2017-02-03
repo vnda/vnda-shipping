155297a6c6d3ed96cf00c9dd4934f49c9db6ef68
Rafael Souza <rafael.ssouza@gmail.com>
2017-02-03 11:22:31 -0200

    add more tests

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -1,20 +1,3 @@
-# == Schema Information
-#
-# Table name: shipping_methods
-#
-#  id               :integer          not null, primary key
-#  shop_id          :integer          not null
-#  name             :string(255)      not null
-#  description      :string(255)      default(""), not null
-#  slug             :string(255)      not null
-#  express          :boolean          default(FALSE), not null
-#  enabled          :boolean          default(FALSE), not null
-#  weigth_range     :numrange         not null
-#  delivery_type_id :integer
-#  data_origin      :string(255)      default("local"), not null
-#  service          :string(255)
-#
-
 class ShippingMethod < ActiveRecord::Base
 
   belongs_to :shop
@@ -76,7 +59,7 @@ class ShippingMethod < ActiveRecord::Base
   end
 
   def copy_to_all_shops
-    Shop.all.each do |shop|
+    Shop.find_each do |shop|
       copy = duplicate(shop.id)
       copy.save! unless shop.methods.find_by(name: copy.name)
     end
@@ -85,15 +68,18 @@ class ShippingMethod < ActiveRecord::Base
   def build_or_update_map_rules_from(xml_doc)
     factory = RGeo::Cartesian.factory
 
-    xml_doc.css('Document Folder Placemark').collect do |placemark|
-      name = placemark.css('name').text.strip
-      points = placemark.css('Polygon coordinates').text.split(' ').collect{|z| c = z.split(','); c.pop; c.map(&:to_f)}.collect{|coordinates| factory.point(coordinates[0], coordinates[1])}
-      region = factory.polygon(factory.line_string(points))
+    xml_doc.css('Document Folder Placemark').map do |placemark|
+      points = placemark.css('Polygon coordinates').text.split(' ').map do |z|
+        lat, lon = z.split(",")[0...2].map(&:to_f)
+        factory.point(lat, lon)
+      end
 
-      if map_rule = self.map_rules.where(name: name).first
-        map_rule.update(region: region) if region
-      else
-        map_rule = self.map_rules.create!(name: name, price: nil, deadline: 0, region: region) if region
+      region = factory.polygon(factory.line_string(points))
+      if region
+        name = placemark.css('name').text.strip
+        map_rule = map_rules.find_or_initialize_by(name: name)
+        map_rule.region = region
+        map_rule.save!
       end
 
       map_rule
@@ -101,8 +87,25 @@ class ShippingMethod < ActiveRecord::Base
   end
 
   def check_and_update_places
-    Place.retrieve_from_vnda_places_for(self.shop).each do |place_json|
-      self.places.create(name: place_json['name']) unless self.places.find_by_name(place_json['name'])
+    Place.retrieve_from_vnda_places_for(shop).each do |place_json|
+      places.create(name: place_json['name']) unless places.find_by_name(place_json['name'])
     end
   end
 end
+
+# == Schema Information
+#
+# Table name: shipping_methods
+#
+#  id               :integer          not null, primary key
+#  shop_id          :integer          not null
+#  name             :string(255)      not null
+#  description      :string(255)      default(""), not null
+#  slug             :string(255)      not null
+#  express          :boolean          default(FALSE), not null
+#  enabled          :boolean          default(FALSE), not null
+#  weigth_range     :numrange         not null
+#  delivery_type_id :integer
+#  data_origin      :string(255)      default("local"), not null
+#  service          :string(255)
+#
