cc36d1ad336a6286255367a3dc2a17787bc9efb7
Rafael Souza <rafael.ssouza@gmail.com>
2017-02-08 12:25:30 -0200

    instrument key methods in the quote process

---
+++ config/initializers/rpm_instrumentation.rb
@@ -0,0 +1,30 @@
+if defined?(::NewRelic)
+  Correios.class_eval do
+    include ::NewRelic::Agent::MethodTracer
+    add_method_tracer :quote
+  end
+
+  Intelipost.class_eval do
+    include ::NewRelic::Agent::MethodTracer
+    add_method_tracer :quote
+  end
+
+  Quotations.class_eval do
+    include ::NewRelic::Agent::MethodTracer
+    add_method_tracer :to_a
+  end
+
+  PackageQuotations.class_eval do
+    include ::NewRelic::Agent::MethodTracer
+    add_method_tracer :to_a
+  end
+
+  ZipCodeLocation.class_eval do
+    class << self
+      include ::NewRelic::Agent::MethodTracer
+
+      add_method_tracer :get_geolocation_for
+      add_method_tracer :try_to_create_new_location
+    end
+  end
+end
